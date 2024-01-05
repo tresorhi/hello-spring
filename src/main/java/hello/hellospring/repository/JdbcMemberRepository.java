8dfc8e9a5476c5814645ecc522c8088b324b435a
tresorhi <tresorhi@naver.com>
2024-01-05 16:50:37 +0900

    섹션 6. 스프링 JdbcTemplate

--- src/main/java/hello/hellospring/repository/JdbcMemberRepository.java
+++ src/main/java/hello/hellospring/repository/JdbcMemberRepository.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-@Repository
+//@Repository
 public class JdbcMemberRepository implements MemberRepository {
     private final DataSource dataSource;
     public JdbcMemberRepository(DataSource dataSource) {
